134de000572d3dac19b857338dce9eb396fdc61f
yancieng <yancieng@Yancies-MacBook-Pro.local>
2013-10-16 23:03:18 +1000

    Header + index.php

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
+	<link type="text/css" rel="stylesheet" href="header.css" media="screen" /> 
+	<link type="text/css" rel="stylesheet" href="style.css" media="screen" /> 
+	<!--<script type="text/javascript" src="javascript.js"></script>  -->
+
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Css ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+<style type="text/css">
+
+
+
+
+
+
+
+
+
+
+
+</style>
+
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Javascrpit ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+<script type="text/javascript">
+
+
+
+
+
+
+
+
+
+
+
+</script>
+
+
+
+
+
+	<title>  Team Zen </title>
+
+
+</head>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Content ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+<body>
+
+<?php include 'header.html' ; ?>
+
+
+<div class="bodyContainer">
+<h3>Welcome</h3>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, laboriosam, voluptatem, ipsa vitae maiores quibusdam error deleniti amet voluptatibus nesciunt perspiciatis at dolorem libero mollitia dicta aspernatur provident natus consequuntur inventore eaque quis! Minus, tenetur voluptate obcaecati sint soluta ab fugit enim incidunt beatae perspiciatis rem quia sed possimus. Nemo, eligendi nesciunt ex obcaecati quam porro architecto magnam. Repudiandae, quae, natus aperiam omnis iusto tempore error debitis vitae! Eveniet, illum, facilis animi reiciendis veniam esse fugiat illo soluta explicabo quam incidunt culpa doloribus. Odit, blanditiis, aliquam adipisci quam error in totam maiores inventore est reprehenderit molestiae sed ducimus omnis culpa.
+</p>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
