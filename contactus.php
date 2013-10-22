0a991fb01901858ed0acee6b7f46c7b3e0f7437a
zenfar <zenfar_1991@hotmail.com>
2013-10-22 10:31:29 +1000

    Contact/About & HowItWorks

---
+++ contactus.php
@@ -0,0 +1,48 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<Head>
+		<link type="text/css" rel="stylesheet" href="header.css" media="screen" /> 
+		<link type="text/css" rel="stylesheet" href="style.css" media="screen" />
+		<Title>About Us/Contact</Title>
+	</head>
+	<body>
+		<?php include 'header.html' ; ?>
+		<div class="bodyContainer">
+			<div class="contactUsTitle"><br>
+				<?php
+				echo 'About Us/Contact'
+				?>
+			</div>
+			<br><br>
+			<div class="aboutContent">
+				<?php
+				echo 'Team Zen is an online for parking spaces. We connect homes and 
+				businesses with parking capacity to individuals seeking either 
+				long term or short term parking.<br/><br/>';
+				
+				echo 'This website was first inspired by a friend who had an idea of making
+				the life of the community more convenient. At the same time it also
+				make profit. Those who are away for the week may put up their parking 
+				space in the website for rent. It also benefit those who want to park 
+				around that area to avoid any law violation.<br/><br/>';
+				
+				echo 'This service is made to make both parties convinient as they do not have
+				to meet in order to make arrangement and payment.<br/><br/>';
+				?>
+			</div>
+			<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<img src = "http://storage.canoe.ca/v1/dynamic_resize/sws_path/suns-prod-images/1297454719343_ORIGINAL.jpg?quality=80&size=650x&stmp=1376520699777" 
+			border=0 width=300px height=300px> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<img src= "http://i.dailymail.co.uk/i/pix/2012/10/04/article-2213081-0323DC0F0000044D-999_634x327.jpg"
+			border=0 width=300px height=300px> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<img src="http://www.blogcdn.com/cars.aol.co.uk/media/2013/06/screen-shot-2013-06-19-at-12.08.13.png"
+			border=0 width=300px height=300px>
+			<br><br>
+			<div class="contContent">
+				<?php
+				echo 'For more information you can contact us using the following email:<br/><br/>';
+				echo 'TeamZen@hotmail.com';
+				?>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ howitworks.php
@@ -0,0 +1,29 @@
+html xmlns="http://www.w3.org/1999/xhtml">
+	<Head>
+		<link type="text/css" rel="stylesheet" href="header.css" media="screen" /> 
+		<link type="text/css" rel="stylesheet" href="style.css" media="screen" />
+		<Title>How It Works</Title>
+	</head>
+	<body>
+		<?php include 'header.html' ; ?>
+		<div class="bodyContainer">
+			<div class="howTitle"><br>
+				<?php
+				echo 'How It Works';
+				?>
+			</div>
+			<br><br>
+			<div class="howContent">
+				<?php
+				echo '1. Register as a user.<br/><br/>';
+				echo '2. Choose either to put up the address for renting or rent a parking space.<br/><br/>';
+				echo '3. Under Rent Car Park browse for a parking space and choose your liking.<br/><br/>';
+				echo '4. Pay the user<br/><br/>';
+				echo '5. <br/><br/>';
+				
+				?>
+			</div>
+
+		</div>
+	</body>
+</html>
