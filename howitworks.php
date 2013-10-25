d31160aa2d4157feced02e7cef1d4a15cbf08cc2
zenfar <zenfar_1991@hotmail.com>
2013-10-25 12:51:14 +1000

    editcarpark

--- howitworks.php
+++ howitworks.php
@@ -15,11 +15,19 @@ html xmlns="http://www.w3.org/1999/xhtml">
 			<br><br>
 			<div class="howContent">
 				<?php
+				echo '<b>Register a car park</b><br/><br/>';
 				echo '1. Register as a user.<br/><br/>';
-				echo '2. Choose either to put up the address for renting or rent a parking space.<br/><br/>';
-				echo '3. Under Rent Car Park browse for a parking space and choose your liking.<br/><br/>';
-				echo '4. Pay the user<br/><br/>';
-				echo '5. <br/><br/>';
+				echo '2. Log in using your email and password.<br/><br/>';
+				echo '3. Fill in the details of your car park.<br/><br/>';
+				echo '4. Post it in the website.<br/><br/>';
+				echo '<br/><br/>';
+				
+				echo '<b>Rent a car park</b><br/><br/>';
+				echo '1. Register as a user.<br/><br/>';
+				echo '2. Log in using your email and password.<br/><br/>';
+				echo '3. Under "Rent Car Par", browse the car park of your choice.<br/><br/>';
+				echo '4. Fill in your details.<br/><br/>';
+				echo '5. Make payment.';
 				
 				?>
 			</div>
